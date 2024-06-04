659a98c6e8078805a4d9b444afcab38ab63d8bc3
Dimitris Carvalho Calixto <68653065+dimitriscalixto@users.noreply.github.com>
2024-06-04 18:24:19 -0500

    usando o preventDefault

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,20 @@
 "use client"
 import { CustomButton } from "@/components/CustomButton";
+import { FormEvent } from "react";
 
 
 const Page = () =>  {
-  const handleButton1 = () =>   alert('clicou no botão 1')
-  const handleButton2 = () =>   alert('clicou no botão 2')
-  const handleButton3 = () =>   alert('clicou no botão 3')
+  const handleForSubmit=(event: FormEvent)=>{
+    event.preventDefault();
+    alert("Opa! mandandoo...")
+  }
   return (
-    <div className="w-screen h-screen flex justify-center items-center mr-4">
-      <CustomButton onClick={handleButton1} label="Clique Aqui"/>
-      <CustomButton onClick={handleButton2} label="Clique Aqui 1"/>
-      <CustomButton onClick={handleButton3} label="Clique Aqui 2"/>
+    <div className="w-screen h-screen flex  flex-col justify-center items-center mr-4">
+      <h1 className="text-5xl mb-3">Form de login</h1>
+      <form onSubmit={handleForSubmit}>
+        <input type="text" name="" id="" />
+        <input type="submit" value="Enviar" />
+      </form>
     </div>
   );
 }
