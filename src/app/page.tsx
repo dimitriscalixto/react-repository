fd1ef188d60aaedf7915cce9696b743b8b7fcd13
Dimitris Carvalho Calixto <68653065+dimitriscalixto@users.noreply.github.com>
2024-06-04 17:51:54 -0500

    forma de passar funcao com parametro dentro do onClick

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import { EmojiRating } from "@/components/EmojiRating2";
 import { StudentsTable } from "@/components/StudentTable";
 import { students } from "@/data/students";
-
-export default function Home() {
+"use client"
+export default function Page() {
+  const avisar = (msg: string) => {
+      alert(msg);
+  }
   return (
-    <div className="container mx-auto">
-      <StudentsTable students={students}/>
+    <div className="w-screen h-screen flex justify-center items-center">
+      <button onClick={ () => avisar("mensagem 1")} className="p-3 bg-blue-700 text-white rounded-md mr-3">Clique Aqui 1</button>
+      <button onClick={ () => avisar("mensagem 2")} className="p-3 bg-blue-700 text-white rounded-md">Clique Aqui 2</button>
     </div>
   );
 }
