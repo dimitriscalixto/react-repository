6c098c71c2a22bcd80a59277aab3cfec4437271b
Dimitris Carvalho Calixto <68653065+dimitriscalixto@users.noreply.github.com>
2024-06-04 18:11:03 -0500

    passando eventos via props

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,19 @@
-import { EmojiRating } from "@/components/EmojiRating2";
-import { StudentsTable } from "@/components/StudentTable";
-import { students } from "@/data/students";
 "use client"
-export default function Page() {
-  const avisar = (msg: string) => {
-      alert(msg);
-  }
+import { CustomButton } from "@/components/CustomButton";
+
+
+const Page = () =>  {
+  const handleButton1 = () =>   alert('clicou no botão 1')
+  const handleButton2 = () =>   alert('clicou no botão 2')
+  const handleButton3 = () =>   alert('clicou no botão 3')
   return (
-    <div className="w-screen h-screen flex justify-center items-center">
-      <button onClick={ () => avisar("mensagem 1")} className="p-3 bg-blue-700 text-white rounded-md mr-3">Clique Aqui 1</button>
-      <button onClick={ () => avisar("mensagem 2")} className="p-3 bg-blue-700 text-white rounded-md">Clique Aqui 2</button>
+    <div className="w-screen h-screen flex justify-center items-center mr-4">
+      <CustomButton onClick={handleButton1} label="Clique Aqui"/>
+      <CustomButton onClick={handleButton2} label="Clique Aqui 1"/>
+      <CustomButton onClick={handleButton3} label="Clique Aqui 2"/>
     </div>
   );
 }
+
+
+export default Page;
